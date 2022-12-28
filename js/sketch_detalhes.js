877797f3e9a5f81242625a5bc3c2d88ca691d464
Paula <paulactalim@gmail.com>
2022-12-28 00:03:26 -0300

    separando funcoes

--- js/sketch_detalhes.js
+++ js/sketch_detalhes.js
@@ -1,5 +1,5 @@
 onload = () => {
     const urlParams = new URLSearchParams(location.search);
-    let idJogo = parseInt(urlParams.get('id'))
+    let idJogo = parseInt(urlParams.get('id'))    
     requisicao_games_lancamento_detalhes (idJogo)
 }
--- js/sketch_homepage.js
+++ js/sketch_homepage.js
@@ -32,6 +32,45 @@ function exibir_games_lancamentos(data, filtroBusca) {
     return data;
 }
 
+function exibir_detalhes_games_lancamento (data, id) {
+    let str = ''
+    let i = data.results.findIndex (elem => elem.id == id)
+    
+    if (i != -1) {
+        let jogo = data.results[i]
+        let str = `<div class="Informacoes">
+                        <h1>${jogo.name}</h1>
+                        <div class="especificacoes">
+                            <p><strong>Plataformas disponíveis:</strong> ${jogo.platforms[0].platform.name}`
+        
+        for(let j = 1; j < jogo.platforms.length; j++) {
+            str += `, ${jogo.platforms[j].platform.name}`
+        }
+
+        str += `</p>
+                <p><strong>Gênero:</strong> ${jogo.genres[0].name}`
+
+        for (let j = 1; j < jogo.genres.length; j++) {
+            str+= `, ${jogo.genres[j].name}`
+        }
+
+        str +=         `</p>
+                        <p><strong>Data de lançamento: </strong>${jogo.released}</p>
+                        <p><strong>Avaliação: </strong>${jogo.rating}</p>
+                    </div>
+                </div>
+                <div class="cartaz" style="background-image: url(${jogo.background_image});"><div class="filtro"></div></div>`
+                            
+        document.querySelector('#card').innerHTML = str   
+    }
+    else {
+        document.querySelector('#card').innerHTML = `<div class="jogo-nao-encontrado">
+                                                        <h1>Jogo não encontrado</h1>
+                                                        <a href="./index.html">Voltar para Homepage</a>
+                                                    </div>`
+    }
+}
+
 function requisicao_games_lancamentos(filtroBusca){
     fetch ('https://api.rawg.io/api/games?key=0ae278d26fd24463b3d3c454be18cb17')
         .then(res => res.json ())
@@ -41,44 +80,7 @@ function requisicao_games_lancamentos(filtroBusca){
 function requisicao_games_lancamento_detalhes (id) {
     fetch ('https://api.rawg.io/api/games?key=0ae278d26fd24463b3d3c454be18cb17')
         .then(res => res.json ())
-        .then(data => {
-            let str = ''
-            let i = data.results.findIndex (elem => elem.id == id)
-            
-            if (i != -1) {
-                let jogo = data.results[i]
-                let str = `<div class="Informacoes">
-                                <h1>${jogo.name}</h1>
-                                <div class="especificacoes">
-                                    <p><strong>Plataformas disponíveis:</strong> ${jogo.platforms[0].platform.name}`
-                
-                for(let j = 1; j < jogo.platforms.length; j++) {
-                    str += `, ${jogo.platforms[j].platform.name}`
-                }
-
-                str += `</p>
-                        <p><strong>Gênero:</strong> ${jogo.genres[0].name}`
-
-                for (let j = 1; j < jogo.genres.length; j++) {
-                    str+= `, ${jogo.genres[j].name}`
-                }
-
-                str +=         `</p>
-                                <p><strong>Data de lançamento: </strong>${jogo.released}</p>
-                                <p><strong>Avaliação: </strong>${jogo.rating}</p>
-                            </div>
-                        </div>
-                        <div class="cartaz" style="background-image: url(${jogo.background_image});"><div class="filtro"></div></div>`
-                                    
-                document.querySelector('#card').innerHTML = str   
-            }
-            else {
-                document.querySelector('#card').innerHTML = `<div class="jogo-nao-encontrado">
-                                                                <h1>Jogo não encontrado</h1>
-                                                                <a href="./index.html">Voltar para Homepage</a>
-                                                            </div>`
-            }
-        })
+        .then(data => exibir_detalhes_games_lancamento(data, id))
 }
 
 onload = () =>{
