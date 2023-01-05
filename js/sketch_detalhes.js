fc3c5c01b45a9337222d4e42f01a9925e1fb5fd5
Paula <paulactalim@gmail.com>
2023-01-05 16:35:53 -0300

    retirada de diretorio test

--- js/sketch_detalhes.js
+++ js/sketch_detalhes.js
@@ -3,6 +3,5 @@ onload = () => {
     let idJogo = parseInt(urlParams.get('id'))
     let num_url = parseInt(urlParams.get('num'))
     let complemento = urlParams.get('adicional')
-    console.log(complemento)
     requisicao_games_lancamento_detalhes (idJogo, num_url, complemento)
 }
