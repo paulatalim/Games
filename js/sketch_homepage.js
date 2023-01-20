2262a0dc2ad18b4edbe993d959ad67a4fe2a703c
Paula <paulactalim@gmail.com>
2023-01-20 17:50:00 -0300

    reformulando carousel publisher

--- js/sketch_homepage.js
+++ js/sketch_homepage.js
@@ -353,37 +353,35 @@ function exibir_publisher () {
 
             //Inclusao de novo slide no corrousel
             for (let i = 0; i < Math.ceil(data.results.length/4); i++){
-                str += `<div class="carousel-item row no-gutters`
+                str += `<div class="carousel-item`
 
                 if (i == 0) {
-                    str += ` active">`
-                } else {
-                    str += `">`
+                    str += ` active`
                 }
+                
+                str += `"><div class="publisher-slide">`
+                
+
 
                 for (let j = 0; j < 4; j++) {
                     let jogo = data.results[index]
                     if (index < data.results.length) {
-                        str += `<div class="col-12 col-md-6 col-lg-3 float-left">
-                                    <div class="publisher-slide">
-                                        <div class="publisher-card">
-                                            <div class="publisher-card-banner" style="background-image: url(${jogo.image_background});">
-                                                <div class="publisher-card-banner-filtro">
-                                                    <h2>${jogo.name}</h2>
-                                                </div>
-                                            </div>
+                        str += `<div class="col-12 col-sm-6 col-md-6 col-lg-3 publisher-area-card">
+                                    <div class="publisher-card">
+                                        <div class="publisher-card-banner" style="background-image: linear-gradient(rgba(0,0,0,0) 0%, rgba(0,0,0,1) 70%, rgba(0,0,0,1) 100%), url(${jogo.image_background});">
+                                            <h2 id="publisher-card-title">${jogo.name}</h2>
+                                        </div>
+                                        
+                                        <div class="publisher-card-conteudo"> 
+                                            <h5>Jogos:</h5>
                                             
-                                            <div class="publisher-card-conteudo"> 
-                                                <h4>Jogos:</h3>
-                                                
-                                                <ul id="publisher-card-conteudo-topicos">`
+                                            <ul id="publisher-card-conteudo-topicos">`
 
-                                                for (let k = 0; k < jogo.games.length; k++) {
-                                                    str+=`<li>${jogo.games[k].name}</li>`
-                                                }
-                                                    
-                                                str += `</ul>
-                                            </div>
+                                            for (let k = 0; k < jogo.games.length && k < 3; k++) {
+                                                str+=`<li>${jogo.games[k].name}</li>`
+                                            }
+                                                
+                                            str += `</ul>
                                         </div>
                                     </div>
                                 </div>`
@@ -391,12 +389,14 @@ function exibir_publisher () {
                     index++;
                 }
 
-                str += `</div>`
+                str += `</div></div>`
             }
             
-            document.getElementById('publisher-carousel-cards').innerHTML = str
+            document.querySelector('.publisher-area-slide').innerHTML = str;
+            // document.getElementById('publisher-carousel-cards').innerHTML = str
         }); 
 }
+
 /***************
  * REQUISIÇÕES *
  ***************/
